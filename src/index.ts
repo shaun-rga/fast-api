684b4643e4f3ac51f3a72bc1c3c4118c0cbc6f17
shaun-rga <shaun.gavin@rgare.com>
2022-05-16 20:22:47 -0500

    Adding ESLint and Prettier.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import express from 'express';
+import express from 'express'
 
-const app = express();
+const app = express()
 
-app.use(express.json());
+app.use(express.json())
 
 app.get('/', async (req, res) => {
-  res.set('Access-Control-Allow-Origin', '*');
-  const json = { msg: 'Hello World!' };
-  res.send(json);
-});
+  res.set('Access-Control-Allow-Origin', '*')
+  const json = { msg: 'Hello World!' }
+  res.send(json)
+})
 
-app.listen(8080, () => console.log('http://localhost:8080'));
+app.listen(8080, () => console.log('http://localhost:8080'))
